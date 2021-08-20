380f76579478e73f6c7ed8d6b5f3d11137071ef0
Charlie <charlie.davismiller@gmail.com>
2021-08-20 12:17:57 +0100

    added ether favicon and updated app name

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import { QwertyHandler } from "./Components/QwertyHandler";
 const App = () => {
   const [keyPress, setKeyPress] = useState('');
 
-  console.log(keyPress);
-
   return (
     <div className="app-container">
       <QwertyHandler setKeyPress={setKeyPress} />
