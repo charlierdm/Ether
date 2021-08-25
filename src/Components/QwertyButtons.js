a641776d33451ca084ad983be8c60ccb5e4f05c2
Charlie <charlie.davismiller@gmail.com>
2021-08-25 12:48:32 +0100

    installed howler to play audio + converted files to mp3 +  audio samples now playing on keypress

--- src/Components/QwertyButtons.js
+++ src/Components/QwertyButtons.js
@@ -1,25 +1,45 @@
 import "../App.css";
 import { useEffect } from "react";
+import { Howl } from "howler";
+import qwertyAudioA from './qwertyAudioA.mp3';
+import qwertyAudioD from './qwertyAudioD.mp3';
+import qwertyAudioS from './qwertyAudioS.mp3';
 
 export const QwertyButtons = ({ keyPress, setKeyPress }) => {
+  const audioA = new Howl({
+    src: [qwertyAudioA],
+    volume: 0.3
+  });
 
+  const audioD = new Howl({
+    src: [qwertyAudioD],
+    volume: 0.3
+  });
+
+  const audioS = new Howl({
+    src: [qwertyAudioS],
+    volume: 0.3
+  });
+  
+  
   useEffect(() => {
-    document.addEventListener('keydown', e => {
-     setKeyPress(e.key);
-    })
+    document.addEventListener("keydown", (e) => {
+      setKeyPress(e.key);
+      audioS.play();
+    });
 
-    document.addEventListener('keyup', e => {
+    document.addEventListener("keyup", (e) => {
       if (e.key) {
-        setKeyPress("")
+        setKeyPress("");
       }
-    })
-  })
+    });
+  });
 
   return (
     <div className="qwerty-buttons">
-      <button className={keyPress === 'a' ? "press-a" : ""}>A</button>
-      <button className={keyPress === 's' ? "press-s" : ""}>S</button>
-      <button className={keyPress === 'd' ? "press-d" : ""}>D</button>
+      <button className={keyPress === "a" ? "press-a" : ""}>A</button>
+      <button className={keyPress === "s" ? "press-s" : ""}>S</button>
+      <button className={keyPress === "d" ? "press-d" : ""}>D</button>
     </div>
   );
 };
